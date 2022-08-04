c3ea02e67492e2d3a7c9578b83ec144f2e98c923
Cristian <cche@users.noreply.github.com>
2022-08-04 11:32:09 +0200

    install.sh: Correct script names and added gg

--- install.sh
+++ install.sh
@@ -5,6 +5,5 @@ if [[ ! -d ~/.local/share/tmux ]]; then
 fi
 
 cp tmux-cht-commands tmux-cht-languages ~/.local/share/tmux/
-cp tmux-cht.sh tmux-go-session.sh ~/bin/
-cp tmux.conf ~/.config/tmux/tmux.conf
+cp tmux-cht.sh tgs gg ~/bin/
 
