6477cde349248b0fdc908e2f0ac47ad498714607
Cristian <cche@users.noreply.github.com>
2024-04-03 14:37:10 +0200

    Don't use tmux-cht* anymore

--- install.sh
+++ install.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-if [[ ! -d ~/.local/share/tmux ]]; then
-    mkdir ~/.local/share/tmux
-fi
-
-cp tmux-cht-commands tmux-cht-languages ~/.local/share/tmux/
-cp tmux-cht.sh tgs gg ~/bin/
+cp tgs gg ~/bin/
 
